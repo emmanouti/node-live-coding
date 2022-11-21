b7bf4fda9e868418d27d61903ae7f83a358e3dcb
Emma <emma.nouti@gmail.com>
2022-11-21 16:08:35 +0100

    first commit

---
+++ src/entity/Wilder.js
@@ -0,0 +1,18 @@
+const { SCHEMA } = require("sqlite3");
+
+const EntitySchema = require(typeorm).EntitySchema;
+
+module.exports = new EntitySchema({
+    name: "Wilder",
+    columns: {
+        id: {
+            primary: true,
+            type: "int",
+            generated: true,
+        },
+        name: {
+            type: "text",
+        },
+    }
+}
+)
